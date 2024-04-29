8b27bc59ace5e1d463d47ae5c5494730b1352f9d
Reivaxy <reivaxy@users.noreply.github.com>
2024-04-29 22:57:45 +0200

    Now handling reconnecting a client

--- software/esp32/src/BtServer.cpp
+++ software/esp32/src/BtServer.cpp
@@ -12,7 +12,7 @@ public:
     this->type = type;
   }
 
-  //This method not called
+
   void onWrite(BLECharacteristic *characteristic) override
   {
     log_i("Kinetix received a message"); 
@@ -27,10 +27,24 @@ public:
 
 };
 
+class MyServerCallback : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    log_i("Client connected.");
+  }
+
+  void onDisconnect(BLEServer* pServer) {
+    log_i("Client disconnected");
+    // Need to restart advertising to be able to reconnect
+    pServer->getAdvertising()->start();
+  }
+};
+
 BtServer::BtServer(MessageProcessor *_messageProcessor) {
   messageProcessor = _messageProcessor;
   BLEDevice::init("Kinetix");
   BLEServer *pServer = BLEDevice::createServer();
+
+  pServer->setCallbacks(new MyServerCallback());
    
   BLEService *pService = pServer->createService(SERVICE_UUID);
   
@@ -41,13 +55,13 @@ BtServer::BtServer(MessageProcessor *_messageProcessor) {
                                        );
 
   pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
+  pServer->getAdvertising()->start();
   Serial.println("Kinetix now available for BT connection.");
   pMovementCharacteristic->setCallbacks(new CharacteristicCallBack(movement, messageProcessor));
 }
